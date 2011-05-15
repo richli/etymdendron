fad3ea0f0120dd6b70a5e606e77f2ed66da71a76
Richard Lindsley <richli.ff@gmail.com>
2011-05-15 10:59:16 -0600

    Add argument parser
    I don't have any arguments in mind now, but I can add them here later.

--- etymdendron/etymdendron.py
+++ etymdendron/etymdendron.py
@@ -2,5 +2,10 @@
 
 # This file is the main driver for the program
 
+import argparse
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run etymdendron')
+    args = parser.parse_args()
+
     print('Etymdendron!') # placeholder for now
