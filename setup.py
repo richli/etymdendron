f183bb3be37a85be19c8b36adc5508c35ed454ea
Richard Lindsley <richli.ff@gmail.com>
2011-05-15 11:47:49 -0600

    Indentation update, note to self

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 from distutils.core import setup
 #import py2exe # not on linux; save this for later
 
-setup(name='etymdendron',
+setup(  name='etymdendron',
         version='0.01',
         packages=['etymdendron'],
         description=['Etymology tree viewer'],
         author='Rich Lindsley',
         author_email='richli.ff@gmail.com',
         url='github.com/richli/etymdendron')
+
+# Notes to myself:
+# To create the source distribution in dist/, run ./setup.py sdist
