5fa8f184e33aa790d9d27faa114f91d17566ee93
Richard Lindsley <richli.ff@gmail.com>
2011-05-15 11:28:15 -0600

    Initial distutils setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='etymdendron',
+        version='0.01',
+        packages=['etymdendron'],
+        description=['Etymology tree viewer'],
+        author='Rich Lindsley',
+        author_email='richli.ff@gmail.com',
+        url='github.com/richli/etymdendron')
