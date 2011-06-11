d1b52e20acfb7c063b10d256bdc75c239b268fb7
Richard Lindsley <richli.ff@gmail.com>
2011-06-11 15:00:05 -0600

    Outline the program structure
    I also moved the main code to a main() function and then called that.

--- etymdendron/etymdendron.py
+++ etymdendron/etymdendron.py
@@ -2,10 +2,33 @@
 
 # This file is the main driver for the program
 
+###
+# Imports
+import sys
 import argparse
 
-if __name__ == '__main__':
+###
+# Global constants
+
+def main():
+    """ The main routine """
+    ###
+    # Parse arguments
     parser = argparse.ArgumentParser(description='Run etymdendron')
     args = parser.parse_args()
 
-    print('Etymdendron!') # placeholder for now
+    ###
+    # First let's load the XML
+
+    ###
+    # Search for a word
+
+    ###
+    # Display the tree
+
+    ###
+    # That's all!
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
