317a7014a9a493840e776f4ef554237e37e62322
Richard Lindsley <richli.ff@gmail.com>
2011-06-12 11:19:22 -0600

    Add a function to get a number from user
    This is needed for the next function, choose_word_from_many.

--- etymdendron/cli_funcs.py
+++ etymdendron/cli_funcs.py
@@ -1,5 +1,7 @@
 """ This module contains some helper functions needed for the command line """
 
+###
+# Input functions
 def get_search_word():
     """ Just a wrapper to input a search word
     """
@@ -8,6 +10,24 @@ def get_search_word():
     #TODO: Make sure it doesn't contain illegal characters, such as numbers?
     return word
 
+def get_num_choice(prompt='Input: ',min_num=0,max_num=10):
+    """ Get a number choice with prompt, only allow integers 
+        between min_num and max_num
+    """
+    while True:
+        num = raw_input(prompt)
+        try:
+            num = int(num)
+            if min_num <= num <= max_num:
+                return num
+            else:
+                print('{0} is out of range'.format(num))
+                continue
+        except ValueError:
+            print('That is an invalid number')
+
+###
+# Display functions
 def display_tree(tree, word):
     """ For a given word and tree, display the rest of the tree 
     """
