a7d522d0f13bec0dcbc81bf62c1bfe3e47791ace
Richard Lindsley <richli.ff@gmail.com>
2011-06-22 23:03:43 -0600

    Add some more unit tests

--- etymdendron/test_etym.py
+++ etymdendron/test_etym.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
 """ Testing framework for etymdendron 
     Uses the unittest framework in the stdlib
 """
 
 import unittest
+import sys
 from lxml import etree as ET
 import global_opts
 import common_funcs as cf
+import cli_funcs as cli
+import StringIO
 
 class EtymDB(unittest.TestCase):
     """ Various tests for the database """
@@ -24,9 +28,70 @@ class EtymDB(unittest.TestCase):
         """ Test saving the db """
         raise NotImplementedError
 
-    @unittest.skip('Not yet devised the test')
-    def testSearchWord(self):
-        raise NotImplementedError
+class EtymDisplayCLI(unittest.TestCase):
+    dispHorse = u'''Root: khursa (PIE)
+  Child: hors (Old English, "A man-eating beast")
+    Child: *horse*, hors, horce, horsse, horis, hos, ors (Middle English, "A horse that you feed oats")
+      Child: *horse* (Modern English, "A horse that you ride")
+  Child: ros, hros (Old High German, "Eine Bestie")
+    Child: ros (Middle High German, "Eines infizierten Tieres")
+      Child: ross (Modern German, "Eine blutrünstige Monster")'''
+
+    dispRos = u'''Root: khursa (PIE)
+  Child: hors (Old English, "A man-eating beast")
+    Child: horse, hors, horce, horsse, horis, hos, ors (Middle English, "A horse that you feed oats")
+      Child: horse (Modern English, "A horse that you ride")
+  Child: *ros*, hros (Old High German, "Eine Bestie")
+    Child: *ros* (Middle High German, "Eines infizierten Tieres")
+      Child: ross (Modern German, "Eine blutrünstige Monster")'''
+
+    def testSearchHorse(self):
+        """ Tests how many trees 'horse' is found in """
+        db = cf.loadDB(global_opts.WORDS_FILE)
+        num_trees, matched_words = cf.searchDB(db, 'horse')
+        self.assertEqual(num_trees, 1)
+
+    def testDispHorse(self):
+        """ Tests the CLI display of 'horse' """
+        db = cf.loadDB(global_opts.WORDS_FILE)
+        num_trees, matched_words = cf.searchDB(db, 'horse')
+        chosen_root = matched_words[0][0]
+        chosen_word = [match[1] for match in matched_words]
+        # Redirect stdout to a string
+        tree_output = StringIO.StringIO()
+        sys.stdout = tree_output
+        cli.display_tree(chosen_root, chosen_word, 'horse')
+        tree_output_string = tree_output.getvalue()
+        sys.stdout = sys.__stdout__
+        self.maxDiff = None
+        self.assertEqual(tree_output_string.strip(), self.dispHorse)
+
+    def testDispRos(self):
+        """ Tests the CLI display of 'ros' """
+        db = cf.loadDB(global_opts.WORDS_FILE)
+        num_trees, matched_words = cf.searchDB(db, 'ros')
+        chosen_root = matched_words[0][0]
+        chosen_word = [match[1] for match in matched_words]
+        # Redirect stdout to a string
+        tree_output = StringIO.StringIO()
+        sys.stdout = tree_output
+        cli.display_tree(chosen_root, chosen_word, 'ros')
+        tree_output_string = tree_output.getvalue()
+        sys.stdout = sys.__stdout__
+        self.maxDiff = None
+        self.assertEqual(tree_output_string.strip(), self.dispRos)
+
+    def testSearchBiology(self):
+        """ Tests how many trees 'biology' is found in """
+        db = cf.loadDB(global_opts.WORDS_FILE)
+        num_trees, matched_words = cf.searchDB(db, 'biology')
+        self.assertEqual(num_trees, 2)
+
+    def testSearchNonexistant(self):
+        """ Tests how many trees an unknown word is found in """
+        db = cf.loadDB(global_opts.WORDS_FILE)
+        num_trees, matched_words = cf.searchDB(db, 'kumquat')
+        self.assertEqual(num_trees, 0)
     
 if __name__ == '__main__':
     unittest.main()
