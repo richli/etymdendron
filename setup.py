f205a4935f8dbd981dd1960428f3399d8fc0b0d9
Richard Lindsley <richli.ff@gmail.com>
2011-08-25 11:39:19 -0600

    Build and include documentation to source dist
    I also did a few other minor updates to reflect filename changes.

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import shutil
 #import py2exe # not on linux; save this for later
 
+# Create the documentation
+os.chdir('doc')
+os.system('make html')
+os.chdir('..')
+# Move the doc
+shutil.copytree('doc/_build/html', 'documentation')
+
+# Create the distribution
 setup(  name='etymdendron',
         version='0.50',
         packages=['etymdendron'],
         package_dir={'etymdendron': 'etymdendron'},
-        package_data={'etymdendron': ['words.xml','etym.xrc','etym-gui.pyw']},
-        data_files=['TODO'],
+        package_data={'etymdendron': ['words.xml', 'etym.xrc']},
+        data_files=[('.', ['TODO', 'run.pyw'])],
         description=['Etymology tree viewer'],
         author='Rich Lindsley',
         author_email='richli.ff@gmail.com',
         url='http://github.com/richli/etymdendron')
+
+shutil.rmtree('documentation')
 
 # Notes to myself:
 # To create the source distribution in dist/, run ./setup.py sdist
